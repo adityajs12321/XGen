420ac35bdacc21c7f8ee2ed6faed9bfdf9cc4e1a
EmperoarV <77228612+AdityaJS12321@users.noreply.github.com>
2022-02-12 09:03:06 +0530

    Update XGen.py

--- XGen.py
+++ XGen.py
@@ -1,8 +1,12 @@
+import time
+
 a=int(input("Enter half the length of x"))
 p="#"
 for s in range(a):
-    print(s," ",p,sep="",end="")
-    print((a-s),2," ",p,sep="")
+    print(s*" ",p,sep="",end="")
+    print((a-s)*2*" ",p,sep="")
+    time.sleep(0.01)
 for t in range(a,0,-1):
-    print(t," ",p,sep="",end="")
-    print((a-t),2," ",p,sep="")
+    print(t*" ",p,sep="",end="")
+    print((a-t)*2*" ",p,sep="")
+    time.sleep(0.01)
