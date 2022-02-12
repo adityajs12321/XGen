87973b783424647c30c0a1ec9a713c274f02fdac
EmperoarV <77228612+AdityaJS12321@users.noreply.github.com>
2022-02-12 08:47:52 +0530

    Update XGen.py

--- XGen.py
+++ XGen.py
@@ -1,8 +1,8 @@
 a=int(input("Enter half the length of x"))
 p="#"
 for s in range(a):
-    print(s" ",p,sep="",end="")
-    print((a-s)2" ",p,sep="")
+    print(s," ",p,sep="",end="")
+    print((a-s),2," ",p,sep="")
 for t in range(a,0,-1):
-    print(t" ",p,sep="",end="")
-    print((a-t)2" ",p,sep="")
+    print(t," ",p,sep="",end="")
+    print((a-t),2," ",p,sep="")
